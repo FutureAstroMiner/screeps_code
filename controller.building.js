d220dba2602dd57ad6fc3dccc43e8099fa6f695a
FutureAstroMiner <adam@adam-taylor.me.uk>
2017-09-16 21:39:16 +0100

    Extensions are now created in a pattern around the spawn.

--- controller.building.js
+++ controller.building.js
@@ -4,34 +4,50 @@ var buildingController = {
     run: function(room) {
         var x_shaped_construction = [1, 1, 1, 3, 2, 2, 3, 1, 3, 3];
         var possible_extensions = [0, 0, 5, 10, 20, 30, 40, 50, 60];
+        var possible_starts = [0, 0, 1, -4, -4, -4, -4, 0];
+        // console.log("Initilised");
+        if (!extension_layer) {
+            var extension_layer = 0;
+            room.memory.extention_layer = 0;
+        } else {
+            var extension_layer = room.memory.extention_layer;
+        }
         var my_spawner_loc = room.find(FIND_MY_SPAWNS);
+        // console.log("Initilised2");
         if (room.controller.level != Memory.rcl) {
             Memory.rcl = room.controller.level;
             console.log("Controller level changed to ", Memory.rcl);
             var total_extensions = room.find(FIND_STRUCTURES, {
                 filter: structure => { return (structure.structureType == STRUCTURE_EXTENSION); }
             });
+            console.log(total_extensions);
             if (Memory.rcl > 1 && total_extensions.length < possible_extensions[Memory.rcl]) {
                 //spawn extensions
                 // console.log("Started spawning");
                 var extensions_to_spawn = possible_extensions[Memory.rcl] - total_extensions.length;
-                var locx = my_spawner_loc[0].pos.x++;
-                var locy = my_spawner_loc[0].pos.y++;
-                for (i = 0; i < x_shaped_construction.length; i = i + 2) {
-                    console.log("Entered for loop. extensions_to_spawn is " + extensions_to_spawn);
-                    console.log(room.find(FIND_MY_SPAWNS));
-                    console.log(my_spawner_loc[0].x + ":" + my_spawner_loc[0].y);
-                    console.log(locx + ";" + locy);
-                    if (extensions_to_spawn > 0 && room.createConstructionSite(locx + x_shaped_construction[i],
-                            locy + x_shaped_construction[i + 1],
-                            STRUCTURE_EXTENSION) == 0) {
-                        console.log("Attempted to spawn");
-                        extensions_to_spawn--;
+                for (var j = 0; extensions_to_spawn > 0; j++) {
+                    var locx = my_spawner_loc[0].pos.x + possible_starts[extension_layer];
+                    var locy = my_spawner_loc[0].pos.y + possible_starts[extension_layer + 1];
+                    for (var i = 0; i < x_shaped_construction.length; i = i + 2) {
+                        if (extensions_to_spawn > 0) {
+                            var result = room.createConstructionSite(locx + x_shaped_construction[i],
+                                locy + x_shaped_construction[i + 1],
+                                STRUCTURE_EXTENSION) == 0;
+                            if (result == 0) {
+                                extensions_to_spawn--;
+                            } else if (i > 3) {
+                                extension_layer = extension_layer + 2;
+                            }
+                        }
                     }
                 }
+                if (extensions_to_spawn < 1) {
+                    room.memory.build_extension = false;
+                }
+            } else {
+                room.memory.build_extension = false;
             }
         }
-
     }
 };
 
