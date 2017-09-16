d46e3e2dfc8ea2365f7dcae8fa6d331ba5719ee2
FutureAstroMiner <adam@adam-taylor.me.uk>
2017-09-16 15:34:24 +0100

    Updating repairer code so the last thing it will do is construct if it can.

--- role.repairer.js
+++ role.repairer.js
@@ -1,49 +1,62 @@
 var roleRepairer = {
-  /** @param {Creep} creep **/
-  run: function(creep) {
-    if (creep.memory.repairing && creep.carry.energy == 0) {
-      creep.memory.repairing = false;
-    }
-    if (!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.repairing = true;
-    }
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if (creep.memory.repairing && creep.carry.energy == 0) {
+            creep.memory.repairing = false;
+        }
+        if (!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.repairing = true;
+        }
 
-    if (creep.memory.repairing) {
-      var targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: object => object.hits < object.hitsMax / 4
-      });
+        if (creep.memory.repairing) {
+            var targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: object => object.hits < object.hitsMax / 4
+            });
 
-      if (targets) {
-        if (creep.repair(targets) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets);
-        }
-      }
-    } else {
-      var containers = creep.room.find(FIND_STRUCTURES, {
-        filter: structure => {
-          return (
-            structure.structureType == STRUCTURE_CONTAINER &&
-            structure.store[RESOURCE_ENERGY] > creep.carryCapacity
-          );
-        }
-      });
+            if (targets) {
+                if (creep.repair(targets) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets);
+                    creep.say("repairing");
+                }
+            }
+        } else {
+            var containers = creep.room.find(FIND_STRUCTURES, {
+                filter: structure => {
+                    return (
+                        structure.structureType == STRUCTURE_CONTAINER &&
+                        structure.store[RESOURCE_ENERGY] > creep.carryCapacity
+                    );
+                }
+            });
 
-      if (containers.length > 0) {
-        var source = creep.pos.findClosestByPath(containers);
-        if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, { visualizePathStyle: { stroke: "#ffaa00" } });
-          creep.say("container");
+            if (containers.length > 0) {
+                var source = creep.pos.findClosestByPath(containers);
+                if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, { visualizePathStyle: { stroke: "#ffaa00" } });
+                    creep.say("container");
+                }
+            } else {
+                // TODO find closest construction site.
+                var construction = creep.room.find(FIND_CONSTRUCTION_SITES);
+                if (construction.length > 0) {
+                    if (creep.build(construction[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(construction[0], { visualizePathStyle: { stroke: '#ffffff' } });
+                    } else {
+                        if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+                        } else {
+                            var sources = creep.pos.findClosestByPath(FIND_SOURCES);
+                            if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(sources, {
+                                    visualizePathStyle: { stroke: "#ffaa00" }
+                                });
+                                creep.say("source");
+                            }
+                        }
+                    }
+                }
+            }
         }
-      } else {
-        var sources = creep.pos.findClosestByPath(FIND_SOURCES);
-        if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources, {
-            visualizePathStyle: { stroke: "#ffaa00" }
-          });
-          creep.say("source");
-        }
-      }
     }
-  }
 };
 module.exports = roleRepairer;
