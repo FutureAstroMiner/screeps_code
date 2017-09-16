50b24ed6d3fcc71dfda6bd61951fa01621a4ceec
FutureAstroMiner <adam@adam-taylor.me.uk>
2017-09-16 18:47:28 +0100

    Automated construction of first extension construction

--- main.js
+++ main.js
@@ -3,181 +3,160 @@ var roleUpgrader = require("role.upgrader");
 var roleWorker = require("role.worker");
 var roleRepairer = require("role.repairer");
 
+//Should this go in the main Loop???
+
+// Source.prototype.memory = undefined;
+
+// for (var roomName in Game.rooms) { //Loop through all rooms your creeps/structures are in
+//     var room = Game.rooms[roomName];
+//     if (!room.memory.spawns) {
+//         room.memory.spawns = room.find(FIND_MY_SPAWNS); // array of all my spawns in their rooms
+//         var sources = room.find(FIND_SOURCES);
+//         for (var sourse in sources){
+//           //Create a container
+//           var pos_x = sourse.x;
+//           var pos_y = sourse.y;
+//           var sites = sourse.room.lookAtArea(pos_y--, pos_x--, pos_y++, pos_x++)
+
+//           for (s in sites) {
+//             if (s["terrain"] != "wall"){
+//               room.createConstructionSite(s.pos, STRUCTURE_CONTAINER);
+//               break;
+//             }
+//           }
+//         }
+//     }
+// }
+
+
+
 var my_spawner_name = "Spawn1";
 var my_spawner_loc = Game.spawns.Spawn1.pos;
 
+var x_shaped_construction = [1, 1, 1, 3, 2, 2, 3, 1, 3, 3];
+var possible_extensions = [0, 0, 5, 10, 20, 30, 40, 50, 60];
+
+
 module.exports.loop = function() {
-  //clearing memory of dead creeps
-  for (var name in Memory.creeps) {
-    if (!Game.creeps[name]) {
-      delete Memory.creeps[name];
-      console.log("Clearing non-existing creep memory:", name);
+    //clearing memory of dead creeps
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log("Clearing non-existing creep memory:", name);
+        }
     }
     //setting room control level
-    // if (Game.rooms.sim.controller.level != Memory.rcl) {
-    //   Memory.rcl = Game.rooms.sim.controller.level;
-    //   console.log("Controller level changed to ", Memory.rcl);
-    //   if (Memory.rcl > 1) {
-    //     //spawn extensions
-    //     var extensions_to_spawn = Memory.extensions;
-    //     var locx = my_spawner_loc.x;
-    //     var locy = my_spawner_loc.y;
-    //     if (extensions_to_spawn > 0) {
-    //       if (extensions_to_spawn > 0 &&
-    //         Game.rooms.sim.createConstructionSite(
-    //           locx+1,
-    //           locy,
-    //           STRUCTURE_EXTENSION
-    //         ) == 0
-    //       ) {
-    //         Memory.extensions--;
-    //       }
-    //       if (extensions_to_spawn > 0 &&
-    //         Game.rooms.sim.createConstructionSite(
-    //           locx-1,
-    //           locy,
-    //           STRUCTURE_EXTENSION
-    //         ) == 0
-    //       ) {
-    //         Memory.extensions--;
-    //       }
-    //       if (extensions_to_spawn > 0 &&
-    //         Game.rooms.sim.createConstructionSite(
-    //           locx,
-    //           locy+1,
-    //           STRUCTURE_EXTENSION
-    //         ) == 0
-    //       ) {
-    //         Memory.extensions--;
-    //       }
-    //       if (extensions_to_spawn > 0 &&
-    //         Game.rooms.sim.createConstructionSite(
-    //           locx,
-    //           locy-1,
-    //           STRUCTURE_EXTENSION
-    //         ) == 0
-    //       ) {
-    //         Memory.extensions--;
-    //       }
-    //       locx++;
-    //       locy++;
-    //     }
+    for (var roomName in Game.rooms) { //Loop through all rooms your creeps/structures are in
+        var room = Game.rooms[roomName];
+        if (room.controller.level != Memory.rcl) {
+            Memory.rcl = room.controller.level;
+            console.log("Controller level changed to ", Memory.rcl);
+            var total_extensions = room.find(FIND_STRUCTURES, {
+                filter: structure => { return (structure.structureType == STRUCTURE_EXTENSION); }
+            });
+            if (Memory.rcl > 1 && total_extensions.length < possible_extensions[Memory.rcl]) {
+                //spawn extensions
+                var extensions_to_spawn = possible_extensions[Memory.rcl] - total_extensions.length;
+                var locx = my_spawner_loc.x++;
+                var locy = my_spawner_loc.y++;
+                for (i = 0; i < x_shaped_construction.length; i = i + 2) {
+                    if (extensions_to_spawn > 0 &&
+                        room.createConstructionSite(
+                            locx + x_shaped_construction[i],
+                            locy + x_shaped_construction[i + 1],
+                            STRUCTURE_EXTENSION) == 0) {
+                        extensions_to_spawn--;
+                    }
+                }
+            }
+        }
+    }
 
-    //     // var locx = 24;
-    //     // var locy = 23;
-    //     // var site_pos = new RoomPosition(
-    //     //   my_spawner_loc.x,
-    //     //   my_spawner_loc.x,
-    //     //   my_spawner_loc.name
-    //     // );
-    //     var can_spawn_more = true;
-    //     // while (can_spawn_more) {
-    //     locx++;
-    //     var result = Game.rooms.sim.createConstructionSite(
-    //       locx,
-    //       locy,
-    //       STRUCTURE_EXTENSION
-    //     );
-    //     console.log(result);
-    //     if (result == -14) {
-    //       can_spawn_more = false;
-    //     }
-    //     // }
-    //   }
-    // }
-  }
-
-  var upgrader = _.filter(
-    Game.creeps,
-    creep => creep.memory.role == "upgrader"
-  );
-  var worker = _.filter(Game.creeps, creep => creep.memory.role == "worker");
-  // console.log("Workers/Upgraders: " + worker.length + "/" + upgrader.length);
-  var miner = _.filter(Game.creeps, creep => creep.memory.role == "miner");
-  var repairer = _.filter(Game.creeps, creep => creep.memory.role == "repairer");
-
-  if (worker.length < 2) {
-    var newName = Game.spawns[my_spawner_name].createCreep(
-      [WORK, CARRY, MOVE],
-      undefined,
-      { role: "worker" }
+    var upgrader = _.filter(
+        Game.creeps,
+        creep => creep.memory.role == "upgrader"
     );
-    console.log("Spawning new worker: " + newName);
-  } else {
-    if (worker.length >= 2 && upgrader.length < 1) {
-      var newName = Game.spawns[my_spawner_name].createCreep(
-        [WORK, CARRY, MOVE],
-        undefined,
-        { role: "upgrader" }
-      );
-      console.log("Spawning new upgrader: " + newName);
-    } else {
-      if (worker.length < 6) {
+    var worker = _.filter(Game.creeps, creep => creep.memory.role == "worker");
+    // console.log("Workers/Upgraders: " + worker.length + "/" + upgrader.length);
+    var miner = _.filter(Game.creeps, creep => creep.memory.role == "miner");
+    var repairer = _.filter(Game.creeps, creep => creep.memory.role == "repairer");
+
+    if (worker.length < 2) {
         var newName = Game.spawns[my_spawner_name].createCreep(
-          [WORK, CARRY, MOVE],
-          undefined,
-          { role: "worker" }
+            [WORK, CARRY, MOVE],
+            undefined, { role: "worker" }
         );
         console.log("Spawning new worker: " + newName);
-      } else if (miner.length <1) {
-      var newName = Game.spawns[my_spawner_name].createCreep(
-          [WORK, WORK, WORK, WORK, WORK, MOVE],
-          undefined,
-          { role: "miner" }
-        );
-        console.log("Spawning new miner: " + newName);
+    } else {
+        if (worker.length >= 2 && upgrader.length < 1) {
+            var newName = Game.spawns[my_spawner_name].createCreep(
+                [WORK, CARRY, MOVE],
+                undefined, { role: "upgrader" }
+            );
+            console.log("Spawning new upgrader: " + newName);
+        } else {
+            if (worker.length < 6) {
+                var newName = Game.spawns[my_spawner_name].createCreep(
+                    [WORK, CARRY, MOVE],
+                    undefined, { role: "worker" }
+                );
+                console.log("Spawning new worker: " + newName);
+            } else if (miner.length < 1) {
+                var newName = Game.spawns[my_spawner_name].createCreep(
+                    [WORK, WORK, WORK, WORK, WORK, MOVE],
+                    undefined, { role: "miner" }
+                );
+                console.log("Spawning new miner: " + newName);
+            }
+            if (repairer.length < 1) {
+                var newName = Game.spawns[my_spawner_name].createCreep(
+                    [WORK, CARRY, MOVE],
+                    undefined, { role: "repairer" }
+                );
+                console.log("Spawning new repairer: " + newName);
+            }
         }
-        if (repairer.length<1) {
-        var newName = Game.spawns[my_spawner_name].createCreep(
-          [WORK, CARRY, MOVE],
-          undefined,
-          { role: "repairer" }
+    }
+    //   var miner = _.filter(Game.creeps, creep => creep.memory.role == "miner");
+    //   if (miner < 2) {
+    //     var newName = Game.spawns[my_spawner_name].createCreep(
+    //       [WORK, WORK, WORK, WORK, WORK, MOVE],
+    //       undefined,
+    //       { role: "miner" }
+    //     );
+    //     console.log("Spawning new miner: " + newName);
+    //   }
+
+    var tower = Game.getObjectById("8d99f1cfa54380282eb718b6");
+    if (tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(
+            FIND_STRUCTURES, {
+                filter: structure => structure.hits < structure.hitsMax
+            }
         );
-        console.log("Spawning new repairer: " + newName);
-      }
-    } 
-  }
-  //   var miner = _.filter(Game.creeps, creep => creep.memory.role == "miner");
-  //   if (miner < 2) {
-  //     var newName = Game.spawns[my_spawner_name].createCreep(
-  //       [WORK, WORK, WORK, WORK, WORK, MOVE],
-  //       undefined,
-  //       { role: "miner" }
-  //     );
-  //     console.log("Spawning new miner: " + newName);
-  //   }
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
 
-  var tower = Game.getObjectById("8d99f1cfa54380282eb718b6");
-  if (tower) {
-    var closestDamagedStructure = tower.pos.findClosestByRange(
-      FIND_STRUCTURES,
-      {
-        filter: structure => structure.hits < structure.hitsMax
-      }
-    );
-    if (closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
     }
 
-    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (closestHostile) {
-      tower.attack(closestHostile);
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if (creep.memory.role == "worker") {
+            roleWorker.run(creep);
+        }
+        if (creep.memory.role == "upgrader") {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == "miner") {
+            roleMiner.run(creep);
+        }
+        if (creep.memory.role == "repairer") {
+            roleRepairer.run(creep);
+        }
     }
-  }
-
-  for (var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if (creep.memory.role == "worker") {
-      roleWorker.run(creep);
-    }
-    if (creep.memory.role == "upgrader") {
-      roleUpgrader.run(creep);
-    }
-    if (creep.memory.role == "miner") {
-      roleMiner.run(creep);
-    }
-    if (creep.memory.role == "repairer") {
-      roleRepairer.run(creep);
-    }
-  }
 };
