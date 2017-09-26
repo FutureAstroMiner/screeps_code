8e68b3b381867f5f982904e755fe45f829f3a9cf
FutureAstroMiner <adam@adam-taylor.me.uk>
2017-09-26 19:15:18 +0100

    Workers will now also fill towers

--- role.worker.js
+++ role.worker.js
@@ -15,7 +15,8 @@ var roleWorker = {
                     return (
                         (structure.structureType == STRUCTURE_EXTENSION ||
                             structure.structureType == STRUCTURE_CONTAINER ||
-                            structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER) &&
                         structure.energy < structure.energyCapacity
                     );
                 }
