11c40419a624f96b8209bbe166bf7c0f943836ef
FutureAstroMiner <adam@adam-taylor.me.uk>
2018-08-30 17:05:19 +0100

    Commenting out old tower code.

--- main.js
+++ main.js
@@ -132,22 +132,22 @@ module.exports.loop = function() {
     //     console.log("Spawning new miner: " + newName);
     //   }
 
-    var tower = Game.getObjectById("8d99f1cfa54380282eb718b6");
-    if (tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(
-            FIND_STRUCTURES, {
-                filter: structure => structure.hits < structure.hitsMax
-            }
-        );
-        if (closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
+    // var tower = Game.getObjectById("8d99f1cfa54380282eb718b6");
+    // if (tower) {
+    //     var closestDamagedStructure = tower.pos.findClosestByRange(
+    //         FIND_STRUCTURES, {
+    //             filter: structure => structure.hits < structure.hitsMax
+    //         }
+    //     );
+    //     if (closestDamagedStructure) {
+    //         tower.repair(closestDamagedStructure);
+    //     }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
+    //     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    //     if (closestHostile) {
+    //         tower.attack(closestHostile);
+    //     }
+    // }
 
     var towers = Game.rooms[roomName].find(
         FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
